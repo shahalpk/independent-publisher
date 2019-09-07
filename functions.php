c1a77e8fefd62f05e14614ba0d05e321e3e05685
Shahal Tharique <shahalpk@gmail.com>
2019-09-07 23:49:10 +0530

    changes

--- functions.php
+++ functions.php
@@ -203,6 +203,7 @@ function independent_publisher_scripts() {
 
 	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/fonts/genericons/genericons.css', array(), '1' );
     wp_enqueue_style( 'social-icons', get_template_directory_uri() . '/fonts/social-icons/social-logos.css', array(), '1' );
+    wp_enqueue_style( 'graphik', get_template_directory_uri() . '/fonts/Graphik/graphik.css', array(), '1' );
 
 	wp_enqueue_script( 'independent-publisher-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
